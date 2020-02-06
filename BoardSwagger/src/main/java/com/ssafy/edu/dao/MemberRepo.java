3faef3c38f871c1beac7687560d882318ffbe550
이은규 <sixket4765@gmail.com>
2020-02-06 18:01:33 +0900

    20-02-06 dynamic query need

--- BoardSwagger/src/main/java/com/ssafy/edu/dao/MemberRepo.java
+++ BoardSwagger/src/main/java/com/ssafy/edu/dao/MemberRepo.java
@@ -4,12 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.ssafy.edu.dto.Member;
 
 @Repository
-public interface MemberRepo extends JpaRepository<Member, Integer> {
+public interface MemberRepo extends JpaRepository<Member, Integer>, JpaSpecificationExecutor<Member> {
 	Optional<Member> findByEmail(String email);
 	List<Member> findAll();
+	/*
+	 @Query(value = "select * from table_name where case_1=:case_1 and date='2017-04-04' ", nativeQuery=true)
+	List<TableName> findSomeCase(@Param("case_1") String case_1);
+	 */
 }
